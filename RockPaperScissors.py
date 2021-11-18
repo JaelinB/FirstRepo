ec2a576255a932fd9840a0ca45680a9086616dcf
JaelinB <90801821+JaelinB@users.noreply.github.com>
2021-11-18 08:08:54 -0500

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,15 @@
+import random
+computer = "Rock", "Paper", "Scissors"
+
+user = input()
+if user == "Rock":
+    print (random.choice(computer))
+if user == "Paper":
+    print (random.choice(computer))
+if user == "Scissors":
+    print (random.choice(computer))
+    
+    
+
+
+ 
